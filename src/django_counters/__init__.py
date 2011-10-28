71683b8af7e782f0b0a0e4778b90da74cb31d884
Boaz Leskes <1006375+bleskes@users.noreply.github.com>
2011-10-28 08:41:30 +0200

    Added a simple patcher class to allow wrapping code you don't own with events.
    Cleaned up some test logic

--- src/django_counters/__init__.py
+++ src/django_counters/__init__.py
@@ -1,6 +1,8 @@
-import logging
+import django.utils.log
 import re
 from time import sleep
+
+from . import patcher
 import pycounters
 from pycounters.base import THREAD_DISPATCHER, GLOBAL_DISPATCHER, EventLogger
 from pycounters.counters import AverageTimeCounter, ThreadTimeCategorizer, FrequencyCounter, AverageWindowCounter
@@ -8,6 +10,33 @@ from pycounters.reporters import LogReporter, MultiProcessLogReporter
 from pycounters.reporters.base import JSONFileOutputMixin
 import sys
 from django.conf import settings
+import django.db.backends.util
+
+# wrap django db access layer.
+
+@pycounters.report_start_end("db_access")
+def patched_execute(self,*args,**kwargs):
+    return self.cursor.execute(*args,**kwargs)
+
+@pycounters.report_start_end("db_access")
+def patched_executemany(self,*args,**kwargs):
+    return self.cursor.executemany(*args,**kwargs)
+
+
+# we cannot use PyCounters' patching utility as CursorWrapper is a proxy
+django.db.backends.util.CursorWrapper.execute = patched_execute
+django.db.backends.util.CursorWrapper.executemany = patched_executemany
+
+
+# wrap django's internals with events
+DJANGO_EVENTS_SCHEME=[
+        {"class" : "django.db.backends.util.CursorDebugWrapper","method":"execute","event":"db_access"},
+        {"class" : "django.db.backends.util.CursorDebugWrapper","method":"executemany","event":"db_access"},
+        {"class" : "django.template.Template","method":"render","event":"templating"},
+]
+
+patcher.execute_patching_scheme(DJANGO_EVENTS_SCHEME)
+
 
 
 class JSONFileMultiProcessLogReporter(JSONFileOutputMixin,MultiProcessLogReporter):
@@ -60,22 +89,22 @@ def count_view(name,counters=[]):
 from django.conf import settings
 
 reporter=None
-output_log = logging.getLogger(name="counters")
+output_log = django.utils.log.getLogger(name="counters")
 
 if settings.COUNTERS.get("server"):
     klass = JSONFileMultiProcessLogReporter if settings.COUNTERS.get("JSONFile") else MultiProcessLogReporter
     output_log.info("Multiprocess mode: klass: %s",klass)
     reporter=klass(
         collecting_address=settings.COUNTERS["server"],
-        output_log=output_log,debug_log=logging.getLogger(name="pc_multiproc"),
+        output_log=output_log,debug_log=django.utils.log.getLogger(name="pc_multiproc"),
         output_file=settings.COUNTERS.get("JSONFile")
     )
 else:
     klass = JSONFileLogReporter if settings.COUNTERS.get("JSONFile") else LogReporter
-    reporter=klass(output_log=logging.getLogger(name="counters"),
+    reporter=klass(output_log=django.utils.log.getLogger(name="counters"),
                              output_file=settings.COUNTERS.get("JSONFile"))
 
 reporter.start_auto_report(seconds=settings.COUNTERS.get("reporting_interval" ,300))
 
 if settings.COUNTERS.get("debug",False):
-    GLOBAL_DISPATCHER.add_listener(EventLogger(logging.getLogger(name="counters.events"),property_filter="value"))
+    GLOBAL_DISPATCHER.add_listener(EventLogger(django.utils.log.getLogger(name="counters.events"),property_filter="value"))
